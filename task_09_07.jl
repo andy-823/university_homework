8b2eca8c5f574a18c5dd920e94d6f1ff829ab329
Andy <70722697+andy-823@users.noreply.github.com>
2021-06-28 14:37:33 +0300

    Update task_09_07.jl

--- task_09_07.jl
+++ task_09_07.jl
@@ -1,10 +1,13 @@
-# it may work incorrectly with zero rows
 function convert!(a :: Matrix)
     height = length(a[:,1])
     for i in 2 : height
         for j in i : height
             coeff1 = a[i - 1, i - 1]
             coeff2 = a[j, i - 1]
+            if (coeff1 == 0)
+                a[j, :], a[i - 1, :] = a[i - 1, :], a[j, :]
+                coeff1, coeff2 = coeff2, coeff1
+            end
             if (coeff2 == 0) continue end
             a[j, :] -=  a[i - 1, :] * coeff2 / coeff1
         end
