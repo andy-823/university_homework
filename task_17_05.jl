51db4b2bef3384455b208aac372f8fd808c83d08
Andy <70722697+andy-823@users.noreply.github.com>
2021-06-28 19:16:54 +0300

    Add files via upload

---
+++ task_17_05.jl
@@ -0,0 +1,28 @@
+function dfs_spec(graph :: Vector{Vector{T}}, start) where T
+    len = length(graph)
+    used :: Vector{Bool} = []
+    for _ in 1 : len push!(used, false) end
+    point = start
+    stack = [start]
+    while !isempty(stack)
+        point = stack[end]
+        pop!(stack)
+        for item in graph[point]
+            if !used[item]
+                push!(ans, item)
+                push!(stack, item)
+            end
+            used[item] = true
+        end
+    end
+    ans :: Bool = true
+    for item in used ans = ans && item end
+    return ans
+end
+
+function strong(graph :: Vector{Vector{T}}) where T
+    len = length(graph)
+    ans = true
+    for i in 1 : len ans = ans && dfs_spec(graph, i)
+    return ans
+end
