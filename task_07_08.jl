5c892c8aba1cdb179f3c59c4f1af81af9092de67
Andy <70722697+andy-823@users.noreply.github.com>
2021-06-28 10:23:10 +0300

    Add files via upload

---
+++ task_07_08.jl
@@ -0,0 +1,17 @@
+function ext_euc(m, n)
+    u, v :: Int = 0, 0
+
+    a, b = m, n
+    u_a, v_a = 1, 0
+    u_b, v_b = 0, 1
+
+    while b != 0
+        k = div(a,b)
+        a, b = b, a % b 
+        #УТВ: a % b = a-k*b - остаток от деления a на b
+        u, v = u_a, v_a
+        u_a, v_a = u_b, v_b
+        u_b, v_b = u - k * u_b, v - k*v_b
+    end
+    return [u_a, v_a]
+end
